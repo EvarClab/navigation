0cf56db19b8fa7e2024b27f1272ebcbd0b2ab45e
hoi <hoi.heo@evar.co.kr>
2022-06-15 14:02:50 +0900

    각도 변경

--- dwa_local_planner/src/dwa_planner_ros.cpp
+++ dwa_local_planner/src/dwa_planner_ros.cpp
@@ -1,39 +1,39 @@
 /*********************************************************************
-*
-* Software License Agreement (BSD License)
-*
-*  Copyright (c) 2009, Willow Garage, Inc.
-*  All rights reserved.
-*
-*  Redistribution and use in source and binary forms, with or without
-*  modification, are permitted provided that the following conditions
-*  are met:
-*
-*   * Redistributions of source code must retain the above copyright
-*     notice, this list of conditions and the following disclaimer.
-*   * Redistributions in binary form must reproduce the above
-*     copyright notice, this list of conditions and the following
-*     disclaimer in the documentation and/or other materials provided
-*     with the distribution.
-*   * Neither the name of Willow Garage, Inc. nor the names of its
-*     contributors may be used to endorse or promote products derived
-*     from this software without specific prior written permission.
-*
-*  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-*  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-*  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-*  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-*  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-*  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-*  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-*  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-*  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-*  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
-*  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-*  POSSIBILITY OF SUCH DAMAGE.
-*
-* Author: Eitan Marder-Eppstein
-*********************************************************************/
+ *
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2009, Willow Garage, Inc.
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of Willow Garage, Inc. nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *
+ * Author: Eitan Marder-Eppstein
+ *********************************************************************/
 
 #include <dwa_local_planner/dwa_planner_ros.h>
 #include <Eigen/Core>
@@ -49,56 +49,62 @@
 
 #include <nav_core/parameter_magic.h>
 
-//register this planner as a BaseLocalPlanner plugin
+// register this planner as a BaseLocalPlanner plugin
 PLUGINLIB_EXPORT_CLASS(dwa_local_planner::DWAPlannerROS, nav_core::BaseLocalPlanner)
 
-namespace dwa_local_planner {
+namespace dwa_local_planner
+{
 
-  void DWAPlannerROS::reconfigureCB(DWAPlannerConfig &config, uint32_t level) {
-      if (setup_ && config.restore_defaults) {
-        config = default_config_;
-        config.restore_defaults = false;
-      }
-      if ( ! setup_) {
-        default_config_ = config;
-        setup_ = true;
-      }
+  void DWAPlannerROS::reconfigureCB(DWAPlannerConfig &config, uint32_t level)
+  {
+    if (setup_ && config.restore_defaults)
+    {
+      config = default_config_;
+      config.restore_defaults = false;
+    }
+    if (!setup_)
+    {
+      default_config_ = config;
+      setup_ = true;
+    }
 
-      // update generic local planner params
-      base_local_planner::LocalPlannerLimits limits;
-      limits.max_vel_trans = config.max_vel_trans;
-      limits.min_vel_trans = config.min_vel_trans;
-      limits.max_vel_x = config.max_vel_x;
-      limits.min_vel_x = config.min_vel_x;
-      limits.max_vel_y = config.max_vel_y;
-      limits.min_vel_y = config.min_vel_y;
-      limits.max_vel_theta = config.max_vel_theta;
-      limits.min_vel_theta = config.min_vel_theta;
-      limits.acc_lim_x = config.acc_lim_x;
-      limits.acc_lim_y = config.acc_lim_y;
-      limits.acc_lim_theta = config.acc_lim_theta;
-      limits.acc_lim_trans = config.acc_lim_trans;
-      limits.xy_goal_tolerance = config.xy_goal_tolerance;
-      limits.yaw_goal_tolerance = config.yaw_goal_tolerance;
-      limits.prune_plan = config.prune_plan;
-      limits.trans_stopped_vel = config.trans_stopped_vel;
-      limits.theta_stopped_vel = config.theta_stopped_vel;
-      planner_util_.reconfigureCB(limits, config.restore_defaults);
+    // update generic local planner params
+    base_local_planner::LocalPlannerLimits limits;
+    limits.max_vel_trans = config.max_vel_trans;
+    limits.min_vel_trans = config.min_vel_trans;
+    limits.max_vel_x = config.max_vel_x;
+    limits.min_vel_x = config.min_vel_x;
+    limits.max_vel_y = config.max_vel_y;
+    limits.min_vel_y = config.min_vel_y;
+    limits.max_vel_theta = config.max_vel_theta;
+    limits.min_vel_theta = config.min_vel_theta;
+    limits.acc_lim_x = config.acc_lim_x;
+    limits.acc_lim_y = config.acc_lim_y;
+    limits.acc_lim_theta = config.acc_lim_theta;
+    limits.acc_lim_trans = config.acc_lim_trans;
+    limits.xy_goal_tolerance = config.xy_goal_tolerance;
+    limits.yaw_goal_tolerance = config.yaw_goal_tolerance;
+    limits.prune_plan = config.prune_plan;
+    limits.trans_stopped_vel = config.trans_stopped_vel;
+    limits.theta_stopped_vel = config.theta_stopped_vel;
+    planner_util_.reconfigureCB(limits, config.restore_defaults);
 
-      // update dwa specific configuration
-      dp_->reconfigure(config);
+    // update dwa specific configuration
+    dp_->reconfigure(config);
   }
 
   DWAPlannerROS::DWAPlannerROS() : initialized_(false),
-      odom_helper_("odom"), setup_(false) {
-
+                                   odom_helper_("odom"), setup_(false)
+  {
   }
 
   void DWAPlannerROS::initialize(
       std::string name,
-      tf2_ros::Buffer* tf,
-      costmap_2d::Costmap2DROS* costmap_ros) {
-    if (! isInitialized()) {
+      tf2_ros::Buffer *tf,
+      costmap_2d::Costmap2DROS *costmap_ros)
+  {
+    if (!isInitialized())
+    {
 
       ros::NodeHandle private_nh("~/" + name);
       g_plan_pub_ = private_nh.advertise<nav_msgs::Path>("global_plan", 1);
@@ -108,18 +114,18 @@ namespace dwa_local_planner {
       costmap_ros_->getRobotPose(current_pose_);
 
       // make sure to update the costmap we'll use for this cycle
-      costmap_2d::Costmap2D* costmap = costmap_ros_->getCostmap();
+      costmap_2d::Costmap2D *costmap = costmap_ros_->getCostmap();
 
       planner_util_.initialize(tf, costmap, costmap_ros_->getGlobalFrameID());
 
-      //create the actual planner that we'll use.. it'll configure itself from the parameter server
+      // create the actual planner that we'll use.. it'll configure itself from the parameter server
       dp_ = boost::shared_ptr<DWAPlanner>(new DWAPlanner(name, &planner_util_));
 
-      if( private_nh.getParam( "odom_topic", odom_topic_ ))
+      if (private_nh.getParam("odom_topic", odom_topic_))
       {
-        odom_helper_.setOdomTopic( odom_topic_ );
+        odom_helper_.setOdomTopic(odom_topic_);
       }
-      
+
       initialized_ = true;
 
       // Warn about deprecated parameters -- remove this block in N-turtle
@@ -130,66 +136,77 @@ namespace dwa_local_planner {
       nav_core::warnRenamedParameter(private_nh, "acc_lim_trans", "acc_limit_trans");
       nav_core::warnRenamedParameter(private_nh, "theta_stopped_vel", "rot_stopped_vel");
 
-      private_nh.param<double>("path_heading_tolerance", path_heading_tolerance_, 0.2); //SAMUEL  //1.57
+      private_nh.param<double>("path_heading_tolerance", path_heading_tolerance_, 0.7); // SAMUEL  //1.57
 
       dsrv_ = new dynamic_reconfigure::Server<DWAPlannerConfig>(private_nh);
       dynamic_reconfigure::Server<DWAPlannerConfig>::CallbackType cb = boost::bind(&DWAPlannerROS::reconfigureCB, this, _1, _2);
       dsrv_->setCallback(cb);
     }
-    else{
+    else
+    {
       ROS_WARN("This planner has already been initialized, doing nothing.");
     }
   }
-  
-  bool DWAPlannerROS::setPlan(const std::vector<geometry_msgs::PoseStamped>& orig_global_plan) {
-    if (! isInitialized()) {
+
+  bool DWAPlannerROS::setPlan(const std::vector<geometry_msgs::PoseStamped> &orig_global_plan)
+  {
+    if (!isInitialized())
+    {
       ROS_ERROR("This planner has not been initialized, please call initialize() before using this planner");
       return false;
     }
-    //when we get a new plan, we also want to clear any latch we may have on goal tolerances
+    // when we get a new plan, we also want to clear any latch we may have on goal tolerances
     latchedStopRotateController_.resetLatching();
 
     ROS_INFO("Got new plan");
     return dp_->setPlan(orig_global_plan);
   }
 
-  bool DWAPlannerROS::isGoalReached() {
-    if (! isInitialized()) {
+  bool DWAPlannerROS::isGoalReached()
+  {
+    if (!isInitialized())
+    {
       ROS_ERROR("This planner has not been initialized, please call initialize() before using this planner");
       return false;
     }
-    if ( ! costmap_ros_->getRobotPose(current_pose_)) {
+    if (!costmap_ros_->getRobotPose(current_pose_))
+    {
       ROS_ERROR("Could not get robot pose");
       return false;
     }
 
-    if(latchedStopRotateController_.isGoalReached(&planner_util_, odom_helper_, current_pose_)) {
+    if (latchedStopRotateController_.isGoalReached(&planner_util_, odom_helper_, current_pose_))
+    {
       ROS_INFO("Goal reached");
       return true;
-    } else {
+    }
+    else
+    {
       return false;
     }
   }
 
-  void DWAPlannerROS::publishLocalPlan(std::vector<geometry_msgs::PoseStamped>& path) {
+  void DWAPlannerROS::publishLocalPlan(std::vector<geometry_msgs::PoseStamped> &path)
+  {
     base_local_planner::publishPlan(path, l_plan_pub_);
   }
 
-
-  void DWAPlannerROS::publishGlobalPlan(std::vector<geometry_msgs::PoseStamped>& path) {
+  void DWAPlannerROS::publishGlobalPlan(std::vector<geometry_msgs::PoseStamped> &path)
+  {
     base_local_planner::publishPlan(path, g_plan_pub_);
   }
 
-  DWAPlannerROS::~DWAPlannerROS(){
-    //make sure to clean things up
+  DWAPlannerROS::~DWAPlannerROS()
+  {
+    // make sure to clean things up
     delete dsrv_;
   }
 
-
-
-  bool DWAPlannerROS::dwaComputeVelocityCommands(geometry_msgs::PoseStamped &global_pose, geometry_msgs::Twist& cmd_vel) {
+  bool DWAPlannerROS::dwaComputeVelocityCommands(geometry_msgs::PoseStamped &global_pose, geometry_msgs::Twist &cmd_vel)
+  {
     // dynamic window sampling approach to get useful velocity commands
-    if(! isInitialized()){
+    if (!isInitialized())
+    {
       ROS_ERROR("This planner has not been initialized, please call initialize() before using this planner");
       return false;
     }
@@ -203,13 +220,13 @@ namespace dwa_local_planner {
     gettimeofday(&start, NULL);
     */
 
-    //compute what trajectory to drive along
+    // compute what trajectory to drive along
     geometry_msgs::PoseStamped drive_cmds;
     drive_cmds.header.frame_id = costmap_ros_->getBaseFrameID();
-    
+
     // call with updated footprint
     base_local_planner::Trajectory path = dp_->findBestPath(global_pose, robot_vel, drive_cmds);
-    //ROS_ERROR("Best: %.2f, %.2f, %.2f, %.2f", path.xv_, path.yv_, path.thetav_, path.cost_);
+    // ROS_ERROR("Best: %.2f, %.2f, %.2f, %.2f", path.xv_, path.yv_, path.thetav_, path.cost_);
 
     /* For timing uncomment
     gettimeofday(&end, NULL);
@@ -219,26 +236,28 @@ namespace dwa_local_planner {
     ROS_INFO("Cycle time: %.9f", t_diff);
     */
 
-    //pass along drive commands
+    // pass along drive commands
     cmd_vel.linear.x = drive_cmds.pose.position.x;
     cmd_vel.linear.y = drive_cmds.pose.position.y;
     cmd_vel.angular.z = tf2::getYaw(drive_cmds.pose.orientation);
 
-    //if we cannot move... tell someone
+    // if we cannot move... tell someone
     std::vector<geometry_msgs::PoseStamped> local_plan;
-    if(path.cost_ < 0) {
+    if (path.cost_ < 0)
+    {
       ROS_DEBUG_NAMED("dwa_local_planner",
-          "The dwa local planner failed to find a valid plan, cost functions discarded all candidates. This can mean there is an obstacle too close to the robot.");
+                      "The dwa local planner failed to find a valid plan, cost functions discarded all candidates. This can mean there is an obstacle too close to the robot.");
       local_plan.clear();
       publishLocalPlan(local_plan);
       return false;
     }
 
-    ROS_DEBUG_NAMED("dwa_local_planner", "A valid velocity command of (%.2f, %.2f, %.2f) was found for this cycle.", 
+    ROS_DEBUG_NAMED("dwa_local_planner", "A valid velocity command of (%.2f, %.2f, %.2f) was found for this cycle.",
                     cmd_vel.linear.x, cmd_vel.linear.y, cmd_vel.angular.z);
 
     // Fill out the local plan
-    for(unsigned int i = 0; i < path.getPointsSize(); ++i) {
+    for (unsigned int i = 0; i < path.getPointsSize(); ++i)
+    {
       double p_x, p_y, p_th;
       path.getPoint(i, p_x, p_y, p_th);
 
@@ -254,29 +273,30 @@ namespace dwa_local_planner {
       local_plan.push_back(p);
     }
 
-    //publish information to the visualizer
+    // publish information to the visualizer
 
     publishLocalPlan(local_plan);
     return true;
   }
 
-
-
-
-  bool DWAPlannerROS::computeVelocityCommands(geometry_msgs::Twist& cmd_vel) {
+  bool DWAPlannerROS::computeVelocityCommands(geometry_msgs::Twist &cmd_vel)
+  {
     // dispatches to either dwa sampling control or stop and rotate control, depending on whether we have been close enough to goal
-    if ( ! costmap_ros_->getRobotPose(current_pose_)) {
+    if (!costmap_ros_->getRobotPose(current_pose_))
+    {
       ROS_ERROR("Could not get robot pose");
       return false;
     }
     std::vector<geometry_msgs::PoseStamped> transformed_plan;
-    if ( ! planner_util_.getLocalPlan(current_pose_, transformed_plan)) {
+    if (!planner_util_.getLocalPlan(current_pose_, transformed_plan))
+    {
       ROS_ERROR("Could not get local plan");
       return false;
     }
 
-    //if the global plan passed in is empty... we won't do anything
-    if(transformed_plan.empty()) {
+    // if the global plan passed in is empty... we won't do anything
+    if (transformed_plan.empty())
+    {
       ROS_WARN_NAMED("dwa_local_planner", "Received an empty transformed plan.");
       return false;
     }
@@ -285,8 +305,9 @@ namespace dwa_local_planner {
     // update plan in dwa_planner even if we just stop and rotate, to allow checkTrajectory
     dp_->updatePlanAndLocalCosts(current_pose_, transformed_plan, costmap_ros_->getRobotFootprint());
 
-    if (latchedStopRotateController_.isPositionReached(&planner_util_, current_pose_)) {
-      //publish an empty plan because we've reached our goal position
+    if (latchedStopRotateController_.isPositionReached(&planner_util_, current_pose_))
+    {
+      // publish an empty plan because we've reached our goal position
       std::vector<geometry_msgs::PoseStamped> local_plan;
       std::vector<geometry_msgs::PoseStamped> transformed_plan;
       publishGlobalPlan(transformed_plan);
@@ -300,8 +321,10 @@ namespace dwa_local_planner {
           odom_helper_,
           current_pose_,
           boost::bind(&DWAPlanner::checkTrajectory, dp_, _1, _2, _3));
-//SAMUEL - Check if need to correct heading to the path ---------------------------
-    } else if (latchedStopRotateController_.isHeadingCorrectionNeeded(path_heading_tolerance_, &planner_util_, current_pose_)) {
+      // SAMUEL - Check if need to correct heading to the path ---------------------------
+    }
+    else if (latchedStopRotateController_.isHeadingCorrectionNeeded(path_heading_tolerance_, &planner_util_, current_pose_))
+    {
       ROS_WARN_NAMED("dwa_local_planner", "HeadingCorrectionNeeded");
       std::vector<geometry_msgs::PoseStamped> local_plan;
       std::vector<geometry_msgs::PoseStamped> transformed_plan;
@@ -318,12 +341,17 @@ namespace dwa_local_planner {
           odom_helper_,
           current_pose_,
           boost::bind(&DWAPlanner::checkTrajectory, dp_, _1, _2, _3));
-//--------------------------------------------------------------------------------
-    } else {
+      //--------------------------------------------------------------------------------
+    }
+    else
+    {
       bool isOk = dwaComputeVelocityCommands(current_pose_, cmd_vel);
-      if (isOk) {
+      if (isOk)
+      {
         publishGlobalPlan(transformed_plan);
-      } else {
+      }
+      else
+      {
         ROS_WARN_NAMED("dwa_local_planner", "DWA planner failed to produce path.");
         std::vector<geometry_msgs::PoseStamped> empty_plan;
         publishGlobalPlan(empty_plan);
@@ -331,6 +359,5 @@ namespace dwa_local_planner {
       return isOk;
     }
   }
-
 
 };
